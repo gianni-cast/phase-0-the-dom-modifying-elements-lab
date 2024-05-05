0efe63ac065a5abfc82b7450c2e32658d85e7421
Gianni Castellano <gianniccastellano@gmail.com>
2024-05-04 21:42:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.innerHTML = "Gianni Castellano is the champion";
